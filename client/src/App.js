1a723424acd7abfbf3844e56b566b0f179430a0f
Sean Brieffies <Sean.Brieffies1@ibm.com>
2024-01-22 11:54:10 +0000

    Testing change behaviour

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React -AAKLSDJFLKS
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other BLANK Page</Link>
